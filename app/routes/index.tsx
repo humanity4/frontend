d85a4a4c68d931981c9f22510d0c3c73675b84cd
Kevin Hosford <kevehosford@gmail.com>
2022-04-19 22:08:03 -0400

    Move donate form into index.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,28 @@
-import classNames from 'classnames';
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react';
-import { Auth0Profile } from 'remix-auth-auth0';
 import { route } from 'routes-gen';
+import { getDomainUrl, getStripeSession } from '~/utils/stripe.server';
 
 import { CashIcon, PlusIcon } from '@heroicons/react/outline';
-import { Link } from '@remix-run/react';
+import { redirect } from '@remix-run/node';
+import { Form, Link } from '@remix-run/react';
+
+import type { ActionFunction } from '@remix-run/node';
+export const action: ActionFunction = async ({ request }) => {
+  const stripeRedirectUrl = await getStripeSession('10.00', getDomainUrl(request));
+
+  return stripeRedirectUrl ? redirect(stripeRedirectUrl) : null;
+};
 
 export default function Index() {
   return (
     <div className="pt-[40vh] text-center">
-      <Link
-        className="inline-flex items-center px-6 py-12 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        to={route('/projects')}
-      >
-        <CashIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" /> Donate Now
-      </Link>
+      <Form method="post" className="inline-block">
+        <button
+          className="inline-flex items-center px-6 py-12 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          type="submit"
+        >
+          <CashIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" /> Donate Now
+        </button>
+      </Form>
       <Link
         className="ml-4 inline-flex items-center px-6 py-12 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         to={route('/projects/new')}
